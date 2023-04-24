a64515a81e56fb55b7ddcdd9dd04726901126871
conserpark <120622892+conserpark@users.noreply.github.com>
2023-04-24 21:54:01 +0900

    login form

--- app.js
+++ app.js
@@ -1,32 +1,29 @@
-const title = document.querySelector("div.hello:nth-child(2) h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
-function handleTitleClick() {
-  if (title.style.color === "red") {
-    title.style.color = "blue";
-  } else {
-    title.style.color = "red";
-  }
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreetings(username);
 }
 
-function handleWindoResize() {
-  document.body.style.backgroundColor = "tomato";
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handleWindowCopy() {
-  alert("copier!");
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
 }
-
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-  alert("Good!");
-}
-
-title.addEventListener("click", handleTitleClick);
-
-window.addEventListener("resize", handleWindoResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
